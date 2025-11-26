dbf62cc6c57400098781086257d386ca9f0f4f63
j0ander <143350484+j0ander@users.noreply.github.com>
2025-11-26 15:13:52 -0500

    Add files via upload

--- app/src/main/java/com/programacionavanzada/primitivas/Circle.java
+++ app/src/main/java/com/programacionavanzada/primitivas/Circle.java
@@ -9,7 +9,6 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
-
 public class Circle {
 
     private final FloatBuffer vertexBuffer;
@@ -20,12 +19,15 @@ public class Circle {
     private int colorHandle;
 
     static final int COORD_POR_VERTEX = 3;
-    float color[] = {0.00f, 0.25f, 0.25f, 1.0f};
+    float color[] = {0.9f, 0.8f, 0.0f, 1.0f};
     private final int vertexStride = COORD_POR_VERTEX * 4;
     private final int vertexCount;
     private float circleCoords[];
 
     public Circle(float radius, int numPoints) {
+
+        circleCoords = createCircleCoords(radius,numPoints);
+        vertexCount = circleCoords.length/COORD_POR_VERTEX;
 
         ByteBuffer byteBuffer = ByteBuffer.allocateDirect(circleCoords.length * 4);
         byteBuffer.order(ByteOrder.nativeOrder());
@@ -64,18 +66,45 @@ public class Circle {
         coords.add(0.0f); //para el centro en y
         coords.add(0.0f); //para el centro en z
 
-        double angle = 2.0*Math.PI/numPoints; // calculo del angulo dependiendo del numero de puntos
+        double angle = 2.0 * Math.PI / numPoints; // calculo del angulo dependiendo del numero de puntos
 
-        for(int i = 1; i <= numPoints; i++){
-            double angle2 = angle*i;
-            coords.add((float)(radius*Math.cos(angle2)));
-            coords.add((float)(radius*Math.sin(angle2)));
+        for (int i = 0; i <= numPoints; i++) {
+            double angle2 = angle * i;
+            coords.add((float) (radius * Math.cos(angle2)));
+            coords.add((float) (radius * Math.sin(angle2)));
             coords.add(0.0f);
 
         }
 
-        return coords;
+        float[] arrayV = new float[coords.size()];
+        for(int i = 0; i<coords.size(); i++){
+            arrayV[i] = coords.get(i);
+        }
+
+        return arrayV;
     }
 
+    public void draw(){
+        GLES20.glUseProgram(mProgram);
 
+        positionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        GLES20.glEnableVertexAttribArray(positionHandle);
+
+        GLES20.glVertexAttribPointer(
+                positionHandle, //GUARDA LOS DATOS DE LA VARIABLE 'vPosition'
+                COORD_POR_VERTEX, //CADA PUNTO TIENE 3 COORDENADAS (XYZ)
+                GLES20.GL_FLOAT, //Los datos son numeros decimales (floats)
+                false, //No normalizamos los numeros
+                vertexStride, //Cada punto ocupa 12 bytes en la memoria
+                vertexBuffer //Lee los datos de este contenedor de memoria
+        );
+
+        colorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        GLES20.glUniform4fv(colorHandle, 1, color, 0);
+
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_FAN, 0, vertexCount);
+
+        GLES20.glDisableVertexAttribArray(positionHandle);
+
+    }
 }
